69cac0321f349592e5183d558cde8f580283c230
g8000 <gorczix@gmail.com>
2020-03-17 08:41:09 +0100

    Add DbContext class.

---
+++ ServiceStation.Core/Services/ServiceStationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceStation.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceStation.Core.Services
+{
+    public class ServiceStationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
