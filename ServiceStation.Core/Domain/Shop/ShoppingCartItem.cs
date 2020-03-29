fffe228d11a34b64193ffe5198b91208a9aa7384
g8000 <gorczix@gmail.com>
2020-03-29 13:30:28 +0200

    Add Shopping cart item class.

---
+++ ServiceStation.Core/Domain/Shop/ShoppingCartItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceStation.Core.Domain.Shop
+{
+    class ShoppingCartItem
+    {
+        public int ShoppingCartItemId { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+        public string ShoppingCartId { get; set; }
+    }
+}
