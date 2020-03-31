49aa52ba2caf8ef620a542b056faa031f3c55fd5
g1500 <gorczix@gmail.com>
2020-03-31 09:16:55 +0200

    Add shopping cart.

--- ServiceStation/Controllers/ShoppingCartController.cs
+++ ServiceStation/Controllers/ShoppingCartController.cs
@@ -3,14 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ServiceStation.Models;
+using ServiceStation.Services;
+using ServiceStation.ViewModels;
 
 namespace ServiceStation.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        private readonly IProductRepository _productRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IProductRepository productRepository, ShoppingCart shoppingCart)
+        {
+            _productRepository = productRepository;
+            _shoppingCart = shoppingCart;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
         }
+
+        public RedirectToActionResult AddToShoppingCart(int productId)
+        {
+            var selectedProduct = _productRepository.Get(productId);
+
+            if (selectedProduct != null)
+            {
+                _shoppingCart.AddToCart(selectedProduct, 1);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult Redirect(int productId)
+        {
+            var selectedProduct = _productRepository.Get(productId);
+
+            if (selectedProduct != null)
+            {
+                _shoppingCart.RemoveFromCart(selectedProduct);
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
