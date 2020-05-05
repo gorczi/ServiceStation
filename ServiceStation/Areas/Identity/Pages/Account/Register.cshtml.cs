27bca7532529a47318253daebc06b2a99f42e782
g1500 <gorczix@gmail.com>
2020-05-05 06:54:22 +0200

    Update register form.

--- ServiceStation/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ServiceStation/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -61,6 +61,10 @@ namespace ServiceStation.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            public string City { get; set; }
+
+            public string Country { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -75,7 +79,12 @@ namespace ServiceStation.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { 
+                    UserName = Input.Email, 
+                    Email = Input.Email, 
+                    City = Input.City,
+                    Country = Input.Country
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
